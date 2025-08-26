77563245119f78763ab70c1d71d725e17969864c
futureschoolsystem <futureschoolsystem7@gmail.com>
2025-08-27 03:19:25 +0500

    update

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    webpack: (config) => {
-    config.resolve.alias = config.resolve.alias || {};
-    return config;
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;
